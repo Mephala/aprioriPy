5d889df2efe0792eab6bd21372f2f387ecf23820
Gokhan Ozgozen <gokhan.ozgozen@gmail.com>
2019-11-24 01:46:36 +0300

    Optimizing join step.

--- apri.py
+++ apri.py
@@ -85,6 +85,51 @@ def remove_low_freq():
             del supportedItemFreq[low_freq_item]
 
 
+def get_sets_to_join(retrieve_depth):
+    retval = []
+    for item_set in itemFreq:
+        if type(item_set) == frozenset and len(item_set) == retrieve_depth:
+            retval.append(item_set)
+    return retval
+
+
+def find_common_elements(lhs, rhs):
+    if lhs is None or rhs is None:
+        return 0
+
+    if len(lhs) == 0 or len(rhs) == 0:
+        return 0
+
+    retval = []
+
+    for element in lhs:
+        if element in rhs:
+            retval.append(element)
+
+    return retval
+
+
+def find_uncommon_elements(lhs, rhs):
+    if lhs is None or rhs is None:
+        return 0
+
+    if len(lhs) == 0 or len(rhs) == 0:
+        return 0
+
+    retval = []
+
+    if len(lhs) > len(rhs):
+        for element in lhs:
+            if element not in rhs:
+                retval.append(element)
+    else:
+        for element in rhs:
+            if element not in lhs:
+                retval.append(element)
+
+    return retval
+
+
 colCount = df.shape[1]
 print('Running with min support:', minSupport)
 
@@ -111,24 +156,53 @@ depth = 0
 while check_depth_support(depth):
     depth = depth + 1
     print('Creating tables with level:', depth + 1)
-    selfJoinTableResult = []
-    selfJoinTable = self_join_items(depth)
+    if depth == 1:
+        selfJoinTableResult = []
+        selfJoinTable = self_join_items(depth)
 
-    for selfJoinResult in selfJoinTable:
-        selfJoinTableResult.append(selfJoinResult)
+        for selfJoinResult in selfJoinTable:
+            selfJoinTableResult.append(selfJoinResult)
 
-    for selfJoinResultElement in selfJoinTableResult:
-        joined_sample = set()
-        for elementInner in selfJoinResultElement:
-            joined_sample.add(elementInner)
+        for selfJoinResultElement in selfJoinTableResult:
+            joined_sample = set()
+            for elementInner in selfJoinResultElement:
+                joined_sample.add(elementInner)
 
-        selfJoinResultElementFrozenSet = frozenset(joined_sample)
-        selfJoinResultElementFreq = calculate_set_freq(selfJoinResultElementFrozenSet)
-        addKeyToMaps(selfJoinResultElementFrozenSet, selfJoinResultElementFreq)
+            selfJoinResultElementFrozenSet = frozenset(joined_sample)
+            selfJoinResultElementFreq = calculate_set_freq(selfJoinResultElementFrozenSet)
+            addKeyToMaps(selfJoinResultElementFrozenSet, selfJoinResultElementFreq)
 
-    print('Completed creating tables with level:', depth + 1, 'testing support range.')
-    remove_low_freq()
-    # cim cif
+        print('Completed creating tables with level:', depth + 1, 'testing support range.')
+        remove_low_freq()
+    else:
+        sets_to_join = get_sets_to_join(depth)
+        join_set_len = len(sets_to_join)
+
+        if join_set_len < 2:
+            continue
+
+        for i in range(join_set_len - 1):
+            left_side_set = sets_to_join[i]
+            for j in range(1, join_set_len):
+                right_side_set = sets_to_join[j]
+
+                # common elements' size must be depth - 1
+                common_elements = find_common_elements(left_side_set, right_side_set)
+                if len(common_elements) == depth - 1:
+                    uncommon_elements = find_uncommon_elements(left_side_set, right_side_set)
+
+                    for uncommon_element in uncommon_elements:
+                        base_set = set(common_elements)
+                        base_set.add(uncommon_element)
+                        joined_set = frozenset(base_set)
+
+                        joined_set_freq = calculate_set_freq(joined_set)
+                        if joined_set_freq < minSupport:
+                            continue
+
+                        addKeyToMaps(joined_set, joined_set_freq)
+
+
 
 print('Final depth that we can support:', depth)
 
